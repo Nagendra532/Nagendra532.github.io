50ead9a3171405298581e9a4256969eefd4e4248
Nagendra532 <84804087+Nagendra532@users.noreply.github.com>
2023-02-04 16:40:18 +0530

    Add files via upload

--- Module4-Assignment/script.js
+++ Module4-Assignment/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Nag";
-names[1]="nuthan";
-names[2]="siv";
-names[3]="vamsi";
-names[4]="ram";
-names[5]="nagendra";
-names[6]="gopisetty";
-names[7]="nuthan";
-names[8]="prasad";
-names[9]="varun";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
